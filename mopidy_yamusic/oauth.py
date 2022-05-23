5609017f15ae56d93ec8745d0ab5bb5e88be9246
stffart <ystffart@yandex.ru>
2022-05-23 19:44:35 +0300

    OAuth module

---
+++ mopidy_yamusic/oauth.py
@@ -0,0 +1,154 @@
+import uuid
+import requests
+import asyncio
+from threading import Thread
+import logging
+import time
+from yandex_music import Client
+import socket
+from os.path import expanduser
+
+
+
+logger = logging.getLogger(__name__)
+
+class MetaSingleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(MetaSingleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class OAuthManager(metaclass=MetaSingleton):
+
+    #client_id = '887c0db290e5443c896c5e6e41347a52'
+    #client_secret = 'b87caf622cb44c5aafb9179d80bde5e1'
+    client_id = '23cabbbdc6cd418abb4b39c32c41195d'
+    client_secret = '53bc75238f0c4d08a118e51fe9203300'
+
+    client_futures = []
+    _client = None
+    _callbacks = []
+
+    def __init__(self):
+        self.node_id = uuid.getnode()
+        self.device_uuid = uuid.UUID(int=self.node_id)
+        self.device_code = None
+        self.device_name = socket.gethostname()
+        self.registered_time = None
+        self.home = expanduser("~")
+        try:
+          with open(self.home+'/.yatoken',"r") as token_file:
+            self._token = token_file.read()
+            if len(self._token) == 0:
+              self._token = None
+        except:
+            self._token = None
+
+        logger.error(self._token)
+        self.loop = asyncio.new_event_loop()
+        self.loop.set_exception_handler(self.handle_exception)
+        asyncio.run_coroutine_threadsafe(self.try_get_token(),self.loop)
+        self.background_thread = Thread(target=self.start_background_loop, args=(self.loop,), daemon=True)
+        self.background_thread.start()
+        logger.debug("sync coroutine started")
+
+    def handle_exception(self, loop, context):
+      # context["message"] will always be there; but context["exception"] may not
+      logger.error(f"LOOP EXCEPTION")
+      logger.error(context)
+      msg = context.get("exception", context["message"])
+      logger.error(f"Caught exception: {msg}")
+
+    def start_background_loop(self, loop: asyncio.AbstractEventLoop) -> None:
+       try:
+         asyncio.set_event_loop(loop)
+         loop.run_forever()
+       except Exception:
+         logger.error(sys.exc_info())
+
+    def init_service(self):
+      self._likes_cache.loadLikes()
+
+    def add_callback(self, callback):
+      self._callbacks.append(callback)
+
+    def register_device(self):
+      t = int(time.time())
+      if self.registered_time != None:
+        if self.registered_time > t:
+          logger.error('time is not yet elapsed')
+          return self
+      params = {
+      'client_id': self.client_id,
+      'device_id': str(self.device_uuid),
+      'device_name': self.device_name
+      }
+      logger.error(params)
+      res = requests.post('https://oauth.yandex.ru/device/code',data=params,json={}).json()
+      self.device_code = res['device_code']
+      self.verification_url = res['verification_url']
+      self.user_code = res['user_code']
+      self.registered_time = int(time.time())+300
+      logger.error(res)
+      return self
+
+    @asyncio.coroutine
+    def try_get_token(self):
+      token = None
+      while True:
+         logger.error('try get token')
+         token = self.get_token()
+         if token == None:
+           yield from asyncio.sleep(5)
+         else:
+           break
+      logger.error('init client with token '+token)
+      self._client = Client(token).init()
+      logger.error('init finished')
+      for c in self._callbacks:
+        logger.error('enter callback')
+        c(self._client)
+
+      logger.error(self._client)
+      for f in self.client_futures:
+        logger.error('set future result')
+        f.set_result(self._client)
+      logger.error('token get success')
+
+    def get_token(self):
+      if self._token != None:
+         return self._token
+
+      if self.device_code == None:
+         return None
+
+      params = {
+      'client_id': self.client_id,
+      'client_secret': self.client_secret,
+      'grant_type': 'device_code',
+      'code':self.device_code
+      }
+      logger.error(params)
+      res = requests.post('https://oauth.yandex.ru/token',data=params).json()
+      logger.error(res)
+      if 'access_token' in res:
+        self._token = res['access_token']
+        with open(self.home+'/.yatoken',"w") as token_file:
+           token_file.write(self._token)
+      else:
+        self._token = None
+      return self._token
+
+    def get_client(self):
+      client_future = asyncio.Future()
+      if self._token != None:
+        logger.error('create client')
+        self._client = Client(self._token).init()
+      if self._client != None:
+        client_future.set_result(self._client)
+      else:
+        self.client_futures.append(client_future)
+      return client_future
+
+
---
+++ mopidy_yamusic/web.py
@@ -0,0 +1,90 @@
+import logging
+import os
+import pathlib
+import pykka
+from mopidy import core
+from tornado import gen, web, websocket, httpclient, httputil
+logger = logging.getLogger(__name__)
+import json
+import asyncio
+import yandex_music
+from .oauth import OAuthManager
+
+class AuthHandler(web.RequestHandler):
+
+    def initialize(self):
+      pass
+
+    def get(self, path):
+        auth = OAuthManager()
+        token = auth.get_token()
+        if token == None:
+          auth.register_device()
+          self.write(f"<body style=\"font-family: Roboto; font-size: 2em; color: #fbfbfb; background-color: #121212; \"><div style=\"position:absolute; top: 40%; left: 30%;\" >To authorize<br />Enter code <p><strong style=\"padding: 5px; background: #242424\" >{auth.user_code}</strong></p> at <a href={auth.verification_url} >{auth.verification_url}</a></div></body>")
+        else:
+          self.write("<body style=\"font-family: Roboto; font-size: 2em; color: #fbfbfb; background-color: #121212; \"><div style=\"position:absolute; top: 40%; left: 30%;\" >Application authorized.</div></body>")
+        self.set_status(200)
+
+class IndexHandler(web.RequestHandler):
+
+    def initialize(self, core, client, client_future, bitrate):
+        self._core = core
+        self._bitrate = bitrate
+        self._client = client
+        self._client_future = client_future
+
+
+    @gen.coroutine
+    def get(self, path):
+        uri = path
+        range = self.request.headers.get("Range")
+        logger.error(uri)
+        params = uri.split(":")
+        kind = params[1]
+        if kind == 'track':
+          track_id = params[2]
+          uid = f"{track_id}"
+          logger.error(uid)
+          get_info = False
+          while not get_info:
+            try:
+              if self._client == None:
+                if isinstance(self._client_future,asyncio.Future):
+                   self._client = self._client_future.result()
+              infos = self._client.tracks_download_info(uid, get_direct_links=True)
+              get_info = True
+            except:
+              logger.error("Cannot get track link. Retry...")
+              yield asyncio.sleep(5)
+          logger.error(infos)
+          for info in infos:
+              if info.codec == "mp3" and info.bitrate_in_kbps == self._bitrate:
+                  link = info.direct_link
+                  if link != None:
+                    logger.error(link)
+                    client = httpclient.AsyncHTTPClient(defaults=dict(request_timeout=900))
+                    headers = httputil.HTTPHeaders()
+                    if range != None:
+                      headers = httputil.HTTPHeaders({"Range":range})
+                    requests = [
+                       httpclient.HTTPRequest(url=link,headers=headers,streaming_callback=self.on_chunk,header_callback=self.on_headers)
+                    ]
+                    try:
+                      yield list(map(client.fetch,requests))
+                    except:
+                      #retry
+                      yield list(map(client.fetch,requests))
+                    self.finish()
+                    return
+        self.write('Not found')
+
+    def on_headers(self,header):
+         params=header.split(': ')
+         if len(params) == 2:
+           self.set_header(params[0],params[1].replace('\r\n',''))
+         if 'HTTP/1.1 206' in header:
+           self.set_status(206)
+
+    def on_chunk(self, chunk):
+        self.write(chunk)
+        self.flush()
